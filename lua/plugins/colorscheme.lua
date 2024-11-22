836a9193f9b6bbaac2353fd307aeacc6687c3120
Namra Maniar <namra4122@gmail.com>
2024-11-22 23:53:40 +0530

    added github theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,10 +1,24 @@
+-- {
+--   "craftzdog/solarized-osaka.nvim",
+--   lazy = false,
+--   priority = 1000,
+--   opts = function()
+--     return {
+--       transparent = true,
+--     }
+--   end,
+-- }
+
 return {
-  "craftzdog/solarized-osaka.nvim",
-  lazy = false,
-  priority = 1000,
-  opts = function()
-    return {
-      transparent = true,
-    }
+  "projekt0n/github-nvim-theme",
+  name = "github-theme",
+  lazy = false, -- make sure we load this during startup if it is your main colorscheme
+  priority = 1000, -- make sure to load this before all the other start plugins
+  config = function()
+    require("github-theme").setup({
+      -- ...
+    })
+
+    vim.cmd("colorscheme github_dark_high_contrast")
   end,
 }
